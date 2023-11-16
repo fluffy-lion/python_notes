398c92a11137da2e09e8f1b052390194d9f27844
fluffy-lion <fluffylionprojects@gmail.com>
2023-11-16 16:50:03 +0000

    variables, expressions and statements

---
+++ main.py
@@ -0,0 +1,58 @@
+# variables, expressions and statements
+
+# CONSTANTS
+# dont change
+# fixed values
+# used to start calculations 
+
+print(123)
+
+# reserved words
+# special words where python is listening to them
+# you cannot use reserved words as variables names / identifiers
+# eg False None True class if lambda continue finally...
+
+# VARIABLES
+# a way to ask python to allocate memory and give it a name 
+# through the assignment statement = 
+
+x = 12
+y = 14
+
+# can change the content of a variable in a later statement
+
+# naming rules
+# must stat with a letter or underscore
+# consiste of letters, numbers and underscore
+# case sensitive
+
+# mnemonic variable names
+# help us remember what we intend to store in them
+# works fine
+x8woehdgcb7 = 35
+x98wjsmwnm3 = 12.50
+print(x8woehdgcb7 * x98wjsmwnm3)
+# better
+a = 35
+b = 12.50
+print(a * b)
+#even better
+hours = 35
+rate = 12.50
+print(hours * rate)
+
+# helps your reader understand whats going on
+
+# x is variable, 2 is constant, = is operator, print is reserved word
+# assignment statement
+x = 2
+# assignment with expression
+x = x + 2
+# print statement
+print(x)
+
+# assignment statements
+# an assignment statement consists of an expression on the right side and a variable to store the result
+# evaluates the right before the left
+# the result is place in the variable identifier 
+
